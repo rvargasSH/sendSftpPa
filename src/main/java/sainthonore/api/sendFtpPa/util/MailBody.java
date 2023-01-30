54bc5cf44461d01d59202ff299766dfee44ee67f
Reynaldo Vargas Gaitan <vargas.reynaldo@sthonore.com.co>
2023-01-30 15:50:39 -0500

    changes to update the queries to send the services sells report

--- src/main/java/sainthonore/api/sendFtpPa/util/MailBody.java
+++ src/main/java/sainthonore/api/sendFtpPa/util/MailBody.java
@@ -11,7 +11,7 @@ public class MailBody {
     public String sellServiceMessage()
             throws UnknownHostException {
         String mailbody = "Buen día<br><br>";
-        mailbody += "Adjunto encontrara el archivo de las ventas de los servicios durante los últimos 180 días.<b><br><br>";
+        mailbody += "Adjunto encontrara el archivo de las ventas de los servicios durante los últimos 30 días.<b><br><br>";
         mailbody += "Atentamente.";
         return mailbody;
     }
